1ee6cfca21c012228585ef113895ed44daafbd9a
SamirFarhat17 <samirfarhat17@gmail.com>
2021-10-10 21:13:34 -0400

    Updated MATLAB classification

---
+++ CW_IMAGES/sample.m
@@ -0,0 +1,169 @@
+ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)
+
+ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)
+ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)
+ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)
+ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)
+ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)
+ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)
+ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)
+ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)
+ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)
+ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)
+ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)
+ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)
+ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)
+
+ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)
+ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)
+ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)
+ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)
+
+
+
+
+ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)ds = tabularTextDatastore('airlinesmall.csv');
+ds.TreatAsMissing = 'NA';
+ds.SelectedVariableNames = {'Year','Month','ArrDelay','DepDelay','Origin','Dest'};
+ds.SelectedFormats(5:6) = {'%C','%C'};
+pre = preview(ds)
